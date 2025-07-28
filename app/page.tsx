9ac29c76f84e2cf2db7a43927b1f2753fddbfd66
Nayan Chowdhury <nayan.1aacl@gmail.com>
2025-07-29 01:04:17 +0600

    Authentication with Better Auth and test implementation

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,58 @@
-import React from "react";
+"use client";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { signUp } from "@/lib/auth-client";
+import React, { useState } from "react";
 
 const Home = () => {
-  return <div>Home</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [name, setName] = useState("");
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    signUp.email(
+      {
+        email,
+        password,
+        name,
+      },
+      {
+        onRequest: (ctx) => {
+          console.log("Loading:", ctx);
+        },
+        onSuccess: (ctx) => {
+          console.log("Success:", ctx);
+        },
+        onError: (ctx) => {
+          console.log("Error:", ctx);
+        },
+      }
+    );
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className="flex flex-col gap-2 p-16">
+      <Input
+        placeholder="Name"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <Input
+        placeholder="Email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <Input
+        placeholder="Password"
+        type="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+
+      <Button type="submit">Submit</Button>
+    </form>
+  );
 };
 
 export default Home;
